12944c8d8beb2523d90a03d49ade6f38d6dc1ecb
pizihao <2335715300@qq.com>
2022-05-21 21:58:36 +0800

    添加reflectasm依赖

--- src/main/java/com/deep/tool/beans/CopierTools.java
+++ src/main/java/com/deep/tool/beans/CopierTools.java
@@ -1,12 +1,15 @@
 package com.deep.tool.beans;
 
 import com.deep.tool.annotations.DerivedTypes;
+import com.esotericsoftware.reflectasm.ConstructorAccess;
 import net.sf.cglib.beans.BeanCopier;
 import net.sf.cglib.core.Converter;
 import net.sf.cglib.core.KeyFactory;
 
-import java.util.Map;
+import java.lang.reflect.Constructor;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 /**
  * <h2></h2>
@@ -74,29 +77,18 @@ public class CopierTools {
     /**
      * <h2>属性copy</h2>
      * 将r(源)中的值覆盖的装入到t(目标)中<br>
-     * 使用转换器
-     *
-     * @param r 源对象
-     * @param t 目标对象
-     * @author Created by liuwenhao on 2022/5/21 17:47
-     */
-    public static void copy(Object r, Object t, Converter converter) {
-        BeanCopier beanCopier = create(r.getClass(), t.getClass(), true);
-        beanCopier.copy(r, t, converter);
-    }
-
-    /**
-     * <h2>属性copy</h2>
-     * 将r(源)中的值覆盖的装入到t(目标)中<br>
      *
      * @param <T>    目标类型
      * @param r      源对象
-     * @param tClazz 目标类实例
+     * @param tClass 目标类实例
      * @return T
      * @author Created by liuwenhao on 2022/5/21 18:04
      */
-    public static <T> T newInstanceCopy(Object r, Class<T> tClazz) {
-        return null;
+    public static <T> T newInstanceCopy(Object r, Class<T> tClass) {
+        ConstructorAccess<T> constructorAccess = ConstructorAccess.get(tClass);
+        T tInstance = constructorAccess.newInstance();
+        copy(r, tInstance);
+        return tInstance;
     }
 
     /**
@@ -114,6 +106,41 @@ public class CopierTools {
     public static <R, T> T copyAcquire(R r, T t) {
         copy(r, t);
         return t;
+    }
+
+    /**
+     * <h2>属性copy</h2>
+     * Collection中每一个元素都转化为目标对象<br>
+     * 使用泛型固定其真实类型
+     *
+     * @param <R>         源对象类型
+     * @param <T>         目标对象类型
+     * @param rCollection 源对象集合
+     * @param tClass      目标类对象
+     * @return java.util.List<T>
+     * @author Created by liuwenhao on 2022/5/21 21:07
+     */
+    public static <R, T> Collection<T> copyCollectionAcquire(Collection<R> rCollection, Class<T> tClass) {
+        ConstructorAccess<T> constructorAccess = ConstructorAccess.get(tClass);
+        return rCollection.stream().map(r -> {
+            T tInstance = constructorAccess.newInstance();
+            copy(r, tInstance);
+            return tInstance;
+        }).collect(Collectors.toList());
+    }
+
+    /**
+     * <h2>属性copy</h2>
+     *
+     * @param <R>       源对象类型
+     * @param <T>       目标对象类型
+     * @param rIterator 源对象集合
+     * @param tClass    目标类对象
+     * @return java.util.Iterator<T>
+     * @author Created by liuwenhao on 2022/5/21 21:23
+     */
+    public static <R, T> Iterator<T> copyIteratorAcquire(Iterator<R> rIterator, Class<T> tClass) {
+        return null;
     }
 
     interface GenerateKey {

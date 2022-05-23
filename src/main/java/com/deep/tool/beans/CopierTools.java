7e753720f2525c0703698107c3b3a3cdc620af60
pizihao <2335715300@qq.com>
2022-05-23 13:13:26 +0800

    CopierTools.java 添加注释

--- src/main/java/com/deep/tool/beans/CopierTools.java
+++ src/main/java/com/deep/tool/beans/CopierTools.java
@@ -6,17 +6,88 @@ import net.sf.cglib.beans.BeanCopier;
 import net.sf.cglib.core.Converter;
 import net.sf.cglib.core.KeyFactory;
 
-import java.lang.reflect.Constructor;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 /**
- * <h2></h2>
+ * <h2>Copier</h2>
+ * 对BeanCopier功能的相对扩展，主要方法全部使用static修饰：
+ * <table>
+ *     <th>
+ *         <td>方法名</td>
+ *         <td>:</td>
+ *         <td>说明</td>
+ *     </th>
+ *     <tr>
+ *         <td>{@link #create(Class, Class)}</td>
+ *         <td>:</td>
+ *         <td>构建不使用转换器的BeanCopier</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@link #create(Class, Class, boolean)}</td>
+ *         <td>:</td>
+ *         <td>构建使用转换器的BeanCopier</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@link #copy(Object, Object)}</td>
+ *         <td>:</td>
+ *         <td>将源对象中的属性值覆盖的装入到目标对象中，不使用转换器</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@link #copy(Object, Object, Converter)}</td>
+ *         <td>:</td>
+ *         <td>将源对象中的属性值覆盖的装入到目标对象中，使用转换器</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@link #copyAcquire(Object, Object)}</td>
+ *         <td>:</td>
+ *         <td>将源对象中的属性值覆盖的装入到目标对象中，并返回目标对象，不使用转换器</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@link #copyAcquire(Object, Object, Converter)}</td>
+ *         <td>:</td>
+ *         <td>将源对象中的属性值覆盖的装入到目标对象中，并返回目标对象，使用转换器</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@link #newInstanceCopy(Object, Class)}</td>
+ *         <td>:</td>
+ *         <td>将源对象中的属性值覆盖的装入到目标对象中，目标对象通过Class类对象创建实例，不使用转换器</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@link #newInstanceCopy(Object, Class, Converter)} </td>
+ *         <td>:</td>
+ *         <td>将源对象中的属性值覆盖的装入到目标对象中，目标对象通过Class类对象创建实例，使用转换器</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@link #copyCollectionAcquire(Collection, Class)}</td>
+ *         <td>:</td>
+ *         <td>Collection中每一个元素都转化为目标对象，目标对象通过Class类对象创建实例，不使用转换器</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@link #copyCollectionAcquire(Collection, Class, Converter)}</td>
+ *         <td>:</td>
+ *         <td>Collection中每一个元素都转化为目标对象，目标对象通过Class类对象创建实例，使用转换器</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@link #copyIteratorAcquire(Iterable, Class)}</td>
+ *         <td>:</td>
+ *         <td>Iterable中每一个元素都转化为目标对象，目标对象通过Class类对象创建实例，不使用转换器</td>
+ *     </tr>
+ *     <tr>
+ *         <td>{@link #copyIteratorAcquire(Iterable, Class, Converter)}</td>
+ *         <td>:</td>
+ *         <td>Iterable中每一个元素都转化为目标对象，目标对象通过Class类对象创建实例，使用转换器</td>
+ *     </tr>
+ * </table>
  *
  * @author Create by liuwenhao on 2022/4/18 4:58
  */
 @DerivedTypes
+@SuppressWarnings("unused")
 public class CopierTools {
 
     /**
@@ -131,16 +202,134 @@ public class CopierTools {
 
     /**
      * <h2>属性copy</h2>
+     * Iterable中每一个元素都转化为目标对象<br>
      *
      * @param <R>       源对象类型
      * @param <T>       目标对象类型
-     * @param rIterator 源对象集合
+     * @param rIterable 可迭代的源对集合
      * @param tClass    目标类对象
      * @return java.util.Iterator<T>
      * @author Created by liuwenhao on 2022/5/21 21:23
      */
-    public static <R, T> Iterator<T> copyIteratorAcquire(Iterator<R> rIterator, Class<T> tClass) {
-        return null;
+    public static <R, T> Iterable<T> copyIteratorAcquire(Iterable<R> rIterable, Class<T> tClass) {
+        ConstructorAccess<T> constructorAccess = ConstructorAccess.get(tClass);
+        List<T> tList = new ArrayList<>();
+        for (R r : rIterable) {
+            T tInstance = constructorAccess.newInstance();
+            copy(r, tInstance);
+            tList.add(tInstance);
+        }
+        return tList;
+    }
+
+    // ==========================useConverter===========================
+
+    /**
+     * <h2>属性copy</h2>
+     * 将r(源)中的值覆盖的装入到t(目标)中<br>
+     * 使用转换器
+     *
+     * @param r         源对象
+     * @param t         目标对象
+     * @param converter 转换器
+     * @author Created by liuwenhao on 2022/5/21 17:47
+     */
+    public static void copy(Object r, Object t, Converter converter) {
+        BeanCopier beanCopier = create(r.getClass(), t.getClass(), true);
+        beanCopier.copy(r, t, converter);
+    }
+
+    /**
+     * <h2>属性copy</h2>
+     * 将r(源)中的值覆盖的装入到t(目标)中<br>
+     * 使用转换器
+     *
+     * @param <T>       目标类型
+     * @param r         源对象
+     * @param tClass    目标类实例
+     * @param converter 转换器
+     * @return T
+     * @author Created by liuwenhao on 2022/5/21 18:04
+     */
+    public static <T> T newInstanceCopy(Object r, Class<T> tClass, Converter converter) {
+        ConstructorAccess<T> constructorAccess = ConstructorAccess.get(tClass);
+        T tInstance = constructorAccess.newInstance();
+        copy(r, tInstance, converter);
+        return tInstance;
+    }
+
+    /**
+     * <h2>属性copy</h2>
+     * 将r(源)中的值覆盖的装入到t(目标)中<br>
+     * 通过泛型来确定源和目标的具体类型<br>
+     * 使用转换器
+     *
+     * @param <R>       源对象类型
+     * @param <T>       目标对象类型
+     * @param r         源对象
+     * @param t         目标对象
+     * @param converter 转换器
+     * @return T
+     * @author Created by liuwenhao on 2022/5/21 17:53
+     */
+    public static <R, T> T copyAcquire(R r, T t, Converter converter) {
+        copy(r, t, converter);
+        return t;
+    }
+
+    /**
+     * <h2>属性copy</h2>
+     * Collection中每一个元素都转化为目标对象<br>
+     * 使用泛型固定其真实类型<br>
+     * 使用转换器
+     *
+     * @param <R>         源对象类型
+     * @param <T>         目标对象类型
+     * @param rCollection 源对象集合
+     * @param tClass      目标类对象
+     * @param converter   转换器
+     * @return java.util.List<T>
+     * @author Created by liuwenhao on 2022/5/21 21:07
+     */
+    public static <R, T> Collection<T> copyCollectionAcquire(
+        Collection<R> rCollection,
+        Class<T> tClass,
+        Converter converter
+    ) {
+        ConstructorAccess<T> constructorAccess = ConstructorAccess.get(tClass);
+        return rCollection.stream().map(r -> {
+            T tInstance = constructorAccess.newInstance();
+            copy(r, tInstance, converter);
+            return tInstance;
+        }).collect(Collectors.toList());
+    }
+
+    /**
+     * <h2>属性copy</h2>
+     * Iterable中每一个元素都转化为目标对象<br>
+     * 使用转换器
+     *
+     * @param <R>       源对象类型
+     * @param <T>       目标对象类型
+     * @param rIterable 可迭代的源对集合
+     * @param tClass    目标类对象
+     * @param converter 转换器
+     * @return java.util.Iterator<T>
+     * @author Created by liuwenhao on 2022/5/21 21:23
+     */
+    public static <R, T> Iterable<T> copyIteratorAcquire(
+        Iterable<R> rIterable,
+        Class<T> tClass,
+        Converter converter
+    ) {
+        ConstructorAccess<T> constructorAccess = ConstructorAccess.get(tClass);
+        List<T> tList = new ArrayList<>();
+        for (R r : rIterable) {
+            T tInstance = constructorAccess.newInstance();
+            copy(r, tInstance, converter);
+            tList.add(tInstance);
+        }
+        return tList;
     }
 
     interface GenerateKey {
